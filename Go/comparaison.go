fee379cb2a76126316253170071f4f43cf4283b7
yasminetarik <155385267+yasminetarik@users.noreply.github.com>
2024-01-12 15:54:37 +0100

    comparaison.go

--- Go/comparaison.go
+++ Go/comparaison.go
@@ -1,9 +1,9 @@
 package main
 
-func compareHashes(pairChan <-chan [2]string, resultChan chan<- string, tabmdp []string) {
+func compareHashes(pairChan <-chan [2]string, resultChan chan<- string, hashed_pwd []string) {
 	for pair := range pairChan {
 		password, hashed := pair[0], pair[1]
-		for _, fileHash := range tabmdp {
+		for _, fileHash := range hashed_pwd {
 			if hashed == fileHash {
 				resultChan <- password
 				return
